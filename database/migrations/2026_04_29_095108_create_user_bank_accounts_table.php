ba1a3dc308d3315d10543171e0961e15cee3b0fb
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-04-29 16:05:19 +0600

    new

--- database/migrations/2026_04_29_095108_create_user_bank_accounts_table.php
+++ database/migrations/2026_04_29_095108_create_user_bank_accounts_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->unsignedBigInteger('payment_method_id')->nullable();
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->nullable();
             $table->foreign('payment_method_id')->nullable();
         });
     }
