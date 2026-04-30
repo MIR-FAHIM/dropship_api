23b6c0c15bc0261bfb1982dec648c1b956e4bd79
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-04-30 17:32:34 +0600

    new

--- routes/api.php
+++ routes/api.php
@@ -100,7 +100,7 @@ Route::prefix('products')->group(function () {
     Route::get('/list-home-product', [ProductController::class, 'listHomeProducts'])->withoutMiddleware('token');
     Route::get('/list/featured', [ProductController::class, 'listFeaturedProducts']);
     Route::get('/list/today-deal', [ProductController::class, 'listTodayDealProducts']);
-    Route::get('/details/{id}', [ProductController::class, 'getProductDetails']);
+    Route::get('/details/{id}', [ProductController::class, 'getProductDetails'])->withoutMiddleware('token');
     Route::post('/update/{id}', [ProductController::class, 'updateProduct']);
     Route::delete('/delete/{id}', [ProductController::class, 'deleteProduct']);
     // Images
