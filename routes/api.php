439bb8a8a310deefe17586fe5c2a0e718458c3d9
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-05-10 10:42:45 +0600

    new

--- routes/api.php
+++ routes/api.php
@@ -276,6 +276,7 @@ Route::prefix('facebook')->group(function () {
 
 Route::prefix('vendors')->group(function () {
     Route::post('/register', [VendorController::class, 'vendorRegister'])->withoutMiddleware('token');
+    Route::get('/list', [VendorController::class, 'getVendorList']);
     Route::get('/vendor/id/{id}', [VendorController::class, 'getVendorId']);
     Route::get('/profile/{id}', [VendorController::class, 'getVendorProfile']);
     Route::put('/update/{id}', [VendorController::class, 'updateVendor']);
